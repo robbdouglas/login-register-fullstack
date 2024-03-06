f14fe9d75610aaf1d980b03bad6129d1536a0d11
robbdouglas <robert.nimmrich@icloud.com>
2024-03-06 08:51:04 +0100

    add basic frontend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import "./db-connect.js";
 
 dotenv.config();
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import "../styles/Login.css";
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
